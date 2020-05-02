16b76ca9a0e46a69f1d821812c5d0b70b217ed4b
andyg <ajgatwork@gmail.com>
2020-05-02 13:08:08 +0100

    Initial Commit

---
+++ com.glover.firstgoTests/com_glover_firstgoTests.swift
@@ -0,0 +1,34 @@
+//
+//  com_glover_firstgoTests.swift
+//  com.glover.firstgoTests
+//
+//  Created by Andrew Glover on 02/05/2020.
+//  Copyright © 2020 Andrew Glover. All rights reserved.
+//
+
+import XCTest
+@testable import com_glover_firstgo
+
+class com_glover_firstgoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
